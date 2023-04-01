0149c59ad9b335a4db5e9f24f85b594822530376
[webleesu] <[websumin@gmail.com]>
2023-04-01 19:50:26 +0900

    더미 배열로 다이어리 형태로 랜더링

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이수민",
+    content: "hi",
+    emotion: 5,
+    // created_date: new Date(), //현재 시간
+    created_date: new Date().getTime(), //milliseconds로 변환
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "hello",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "아무개",
+    content: "bye",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
