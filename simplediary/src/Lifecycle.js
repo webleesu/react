29ce273e8300dd521c52ca30b435806c46897db6
[webleesu] <[websumin@gmail.com]>
2023-04-07 15:28:27 +0900

    useEffect
    mount/unmount

--- simplediary/src/Lifecycle.js
+++ simplediary/src/Lifecycle.js
@@ -1,38 +1,25 @@
 import React, { useEffect, useState } from "react";
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
+const UnMountTest = () => {
   useEffect(() => {
     console.log("Mount!");
+    return () => {
+      // Unmount 시점에 실행되게 됨
+      console.log("Unmount!");
+    };
   }, []);
 
-  useEffect(() => {
-    console.log("Update!");
-  });
+  return <div>UnMount Testing Component</div>;
+};
 
-  useEffect(() => {
-    console.log(`count is update : ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다.");
-      setCount(1);
-    }
-  }, [count]);
-
-  useEffect(() => {
-    console.log(`text is update : ${text}`);
-  }, [text]);
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnMountTest />}
     </div>
   );
 };
