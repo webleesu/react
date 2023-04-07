0dda0bfc9b3b64adfd174de1468d182506e0eaf4
[webleesu] <[websumin@gmail.com]>
2023-04-07 15:23:27 +0900

    useEffect
    컴포넌트가 업데이트 될 때 예제

--- simplediary/src/Lifecycle.js
+++ simplediary/src/Lifecycle.js
@@ -8,6 +8,22 @@ const Lifecycle = () => {
     console.log("Mount!");
   }, []);
 
+  useEffect(() => {
+    console.log("Update!");
+  });
+
+  useEffect(() => {
+    console.log(`count is update : ${count}`);
+    if (count > 5) {
+      alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다.");
+      setCount(1);
+    }
+  }, [count]);
+
+  useEffect(() => {
+    console.log(`text is update : ${text}`);
+  }, [text]);
+
   return (
     <div style={{ padding: 20 }}>
       <div>
